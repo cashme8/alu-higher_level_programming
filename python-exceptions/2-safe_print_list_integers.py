82f2f84ee9691845810a9117c384187c0b482984
cashme8 <kabandagislain440@gmail.com>
2025-07-10 18:05:25 +0000

    2-safe_print_list_integers.py:3:1: E302 expected 2 blank lines, found 1 fixed

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
     v = 0
     for r in range(0, x):
@@ -9,4 +10,4 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             continue
     print("")
-    return (v)
+    return v
